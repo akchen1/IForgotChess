b18bcb2c02133be3aac7d15047b55a13d8682ff2
mingyaang <mmao@ualberta.ca>
2018-11-25 23:17:34 -0700

    playing with lower res, switched the board layout to real chess and included a file where we can write scripts for EU pieces

---
+++ euPieces.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+
+class pawn:
+    def __init__(self, tile, display):
+        self.x = tile.x_pos
+        self.y = tile.y_pos
+        self.tile = tile
+        self.image = pygame.image.load('pawn.png')
+        self.image = pygame.transform.scale(self.image, (100,125))
+        display.blit(self.image,(self.x + 35, self.y + 20))
+    def move(self, n_tile, display):
+        self.tile.draw_tile()
+        self.tile = n_tile
+        display.blit(self.image,(n_tile.x_pos + 35, n_tile.y_pos + 20))
